7211928919a1db3b2c99e013d64425eaee0c5598
blueme0 <ksytpdus0304@gmail.com>
2023-08-15 06:56:25 +0900

    add: kt -> dart로 데이터 전송 시 필요한 자료형 추가

---
+++ konkukHackerton_android/android/app/src/main/kotlin/com/example/eom_fe/data/ChallengeMakeData.kt
@@ -0,0 +1,6 @@
+package com.example.eom_fe.data
+
+data class ChallengeMakeData(
+    val challengeRequestData: ChallengeRequestData,
+    val validatorNameList: List<String>
+) : java.io.Serializable
